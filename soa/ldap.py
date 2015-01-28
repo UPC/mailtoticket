a3f69d07a8daac90fd064d47c17ad8d7794d376d
jaumem@fib.upc.edu <jaumem@fib.upc.edu>
2015-01-28 16:29:33 +0100

    Funcionen els testos a produccio

--- soa/ldap.py
+++ soa/ldap.py
@@ -4,12 +4,16 @@ import settings
 class GestioLDAP:
 
   def obtenir_uid(self,mail):
-    p = subprocess.Popen(settings.busca_mail +" "+ mail, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    result=None
-    for line in p.stdout.readlines():
-      result=line
-    if result!=None:
-      result=result.rstrip()
-    else:
-      result=settings.mails_addicionals[mail]
-    return result
+    try:
+        p = subprocess.Popen(settings.busca_mail +" "+ mail, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result=None
+        for line in p.stdout.readlines():
+           result=line
+        if result!=None:
+           result=result.rstrip()
+        else:
+           result=settings.mails_addicionals[mail]
+    except:
+        result=None
+    finally:
+        return result
