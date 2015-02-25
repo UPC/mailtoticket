3fa195a804fb12b3e5875f208cdce8dafb92f6f8
1003001 <jaumem@fib.upc.edu>
2015-02-25 15:46:18 +0100

    Canvis al busca_mail

--- busca_mail.sh
+++ busca_mail.sh
@@ -3,5 +3,7 @@
 # Busca com a mail i com a forward (per aixi tenir mes mails d'estudiants)
 # Tambe busca com mails @upc.edu agafant la primera part del mail per veure si es directament un username encara que tingui un altre mail
 source "/home2/users/lcfib/lcfib-proves/mailtoticket/env.sh"
-possible_username=`echo $1 | sed -e 's/@upc.edu//'`
+mail=`echo $1 | sed -e 's/upc.es/upc.edu/'`
+mail=`echo ${mail} | sed -e 's/@lsi.upc.edu/@cs.upc.edu/'`
+possible_username=`echo ${mail} | sed -e 's/@upc.edu//'`
 ldapsearch -x -h ldap1.fib.upc.es -p 9388 -b "dc=FIB" -w ${ldap_password} -D "${ldap_username}" "(|(mail=$1)(forward=$1)(uid-web=${possible_username}))" | grep "^uid-web:" | cut -d" " -f2
