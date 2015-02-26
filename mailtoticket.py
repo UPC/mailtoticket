dc735292cc6f7528e203a6734095f03424be74c6
1003001 <jaumem@fib.upc.edu>
2015-02-26 11:52:59 +0100

    Missatge quan peta

--- mailtoticket.py
+++ mailtoticket.py
@@ -30,4 +30,5 @@ if __name__ == '__main__':
     if filtres.aplicar_filtres(mail):
       print "x-mailtoticket: afd25dad494b9345fa2e0a34dc2aa4c11594c3e7b672f772a7fa003ad80bd09f045a170213ae2ba4f47eb8043ac61a56e44ff031a014b82f7508bc5543960138"
   finally:
+    logger.error("Ha petat algun dels filtres i no marco el mail com a tractat")
     print mail
