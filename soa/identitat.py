f01d2e6a2cf1bef37dd52b421ac6fa2de7bba521
Jaume Moral <jaumem@fib.upc.edu>
2015-09-02 16:13:52 +0200

    Faig que si un usuari respon per diversos mails amb diferents usernames, intenti retornar el correcte

--- soa/identitat.py
+++ soa/identitat.py
@@ -7,17 +7,35 @@ class GestioIdentitat(SOAService):
 
   mails_addicionals=settings.get("mails_addicionals")
 
-  def obtenir_uid(self,mail):  
+  def canonicalitzar_mail(self,mail):
     mail_canonic=mail.lower()
     mail_canonic=mail_canonic.replace(".upc.es",".upc.edu")
-    mail_canonic=mail_canonic.replace("@lsi","@cs")
+    mail_canonic=mail_canonic.replace("@lsi","@cs")    
+    return mail_canonic
+
+  def obtenir_uid(self,mail):  
+    mail_canonic=self.canonicalitzar_mail(mail)
     uid=None
     try:
       uid=self.mails_addicionals[mail_canonic]      
     except:
       try:
         resultat=self.client.service.llistaPersones(email=mail_canonic)
-        uid=resultat.llistaPersones.persona[0].cn  
+        if len(resultat.llistaPersones.persona)==1:
+          # Quan tenim un resultat, es aquest
+          uid=resultat.llistaPersones.persona[0].cn  
+        else:
+          # Si tenim mes d'un, busquem el que te el mail que busquem com a preferent o be retornem el primer
+          for persona in resultat.llistaPersones.persona:
+            print "busco:"
+            print persona
+            dades_persona=self.client.service.obtenirDadesPersona(commonName=persona.cn)
+            print dades_persona
+            if (self.canonicalitzar_mail(dades_persona.emailPreferent)==mail_canonic):
+              uid=persona.cn
+            print uid
+          if uid==None:
+            resultat.llistaPersones.persona[0].cn
       except:
         uid=None
     finally:
