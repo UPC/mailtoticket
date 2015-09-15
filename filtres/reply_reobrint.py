3217e950e20d3124fcd14f655cea94d3d3c31199
unknown <jaumem@JAKARTA.tolkien.fib.upc.es>
2015-09-15 10:12:57 +0200

    No miro be l'estat del ticket

--- filtres/reply_reobrint.py
+++ filtres/reply_reobrint.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 class FiltreReplyReobrint(FiltreReply):
 
   def filtrar(self):
+  	logger.info ("El ticket esta [%s]" % self.ticket['estat']);
     if self.ticket['estat']=='TIQUET_STATUS_TANCAT':
     	logger.info ("Reobrim el ticket tancat %s" % self.ticket_id);
         resultat=self.tickets.modificar_tiquet(
