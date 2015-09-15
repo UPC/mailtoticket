fae943c7f4066e720f6a204177b7ae69d1276aab
unknown <jaumem@JAKARTA.tolkien.fib.upc.es>
2015-09-15 10:14:25 +0200

    Error

--- filtres/reply_reobrint.py
+++ filtres/reply_reobrint.py
@@ -7,9 +7,9 @@ logger = logging.getLogger(__name__)
 class FiltreReplyReobrint(FiltreReply):
 
   def filtrar(self):
-  	logger.info ("El ticket esta [%s]" % self.ticket['estat']);
+    logger.info ("El ticket esta [%s]" % self.ticket['estat']);
     if self.ticket['estat']=='TIQUET_STATUS_TANCAT':
-    	logger.info ("Reobrim el ticket tancat %s" % self.ticket_id);
+        logger.info ("Reobrim el ticket tancat %s" % self.ticket_id);
         resultat=self.tickets.modificar_tiquet(
             codiTiquet=self.ticket_id,
             estat='TIQUET_STATUS_OBERT'
