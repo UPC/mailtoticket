c24ad503558a25b6295c4fc9fb0090b4f631e1be
Jaume Moral <jaumem@fib.upc.edu>
2015-11-16 18:01:43 +0100

    Arreglem el bug de tickets que no es poden reobrir

--- filtres/reply_reobrint.py
+++ filtres/reply_reobrint.py
@@ -7,17 +7,17 @@ logger = logging.getLogger(__name__)
 class FiltreReplyReobrint(FiltreReply):
 
   def es_aplicable(self):
-  	if not FiltreReply.es_aplicable(self):
-  	  return False
+    if not FiltreReply.es_aplicable(self):
+      return False
 
-  	# Si esta tancat, mirem de reobrir i si no podem, retornem False
+    # Si esta tancat, mirem de reobrir i si no podem, retornem False
 
     if self.ticket['estat']=='TIQUET_STATUS_TANCAT':
       logger.info ("Reobrim el ticket tancat %s" % self.ticket_id)
       resultat=self.tickets.modificar_tiquet(
         codiTiquet=self.ticket_id,
         estat='TIQUET_STATUS_OBERT'
-      )      
+      )
       self.ticket=self.tickets.consulta_tiquet(codi=self.ticket_id)
       if self.ticket['estat']=='TIQUET_STATUS_TANCAT':
         logger.info ("No podem reobrir el ticket %s. El filtre no es aplicable" % self.ticket_id)
