987d47d53196768fab9126ba907afbe0d388b613
Alex Muntada <alexm@alexm.org>
2015-12-17 20:48:37 +0100

    Afegeix la documentació bàsica

---
+++ README.md
@@ -0,0 +1,69 @@
+MailToTicket
+============
+
+Una pasarel·la de correu per al gestor de tiquets GN6.
+
+Requisits
+---------
+
+Aquest programa necessita els següents serveis SOA de la UPC:
+
+*   [Identitat Digital](https://bus-soa.upc.edu/GestioIdentitat/Personesv6?wsdl): permet identificar els sol·licitants amb el correu
+*   [GN6](https://bus-soa.upc.edu/gN6/GestioTiquetsv2?wsdl): permet gestionar els tiquets
+
+Us caldrà disposar d'un usuari i contrasenya per accedir al bus SOA.
+L'usuari i contrasenya del servei GN6 els podeu definir a la vostra instància.
+
+El llenguatge de programació triat és Python, vegeu el fitxer de [dependències](requirements.txt).
+
+Per poder utilitzar aquest programa necessitareu configurar un _Mail Delivery Agent_ (MDA) al vostre servidor de correu o a la bústia IMAP corresponent.
+
+Instal·lació
+------------
+
+La forma recomanada d'instal·lació és dins d'un _virtualenv_ de Python per poder gestionar les dependències sense permisos d'administrador:
+
+```
+git clone git@github.com:UPC/mailtoticket.git
+cd mailtoticket
+virtualenv local
+./local/bin/pip install -r requirements.txt
+```
+
+Configuració de la bústia
+-------------------------
+
+El programa funciona com un filtre de correu i s'ha d'executar per cada correu que es vulgui processar mitjançant un MDA.
+Podeu utilitzar MDA coneguts com ara maildrop o procmail. Vegeu els exemples documentats al wiki.
+
+Configuració de la instància
+----------------------------
+
+Heu de crear el fitxer `settings_default.py` a partir del [settings_sample.py](settings_sample.py) per configurar la vostra instància.
+
+Si voleu tenir diferents instàncies del programa, podeu crear fitxers de configuració amb noms diferents.
+Per llegir-los només cal que ho indiqueu amb l'opció `-c` tot indicant el nom del fitxer sense l'extensió .py:
+
+```
+cat correu.txt | ./mailtoticket.py -c settings_alternatius
+```
+
+Filtres i accions
+-----------------
+
+El comportament del programa és executar una sèrie de filtres sobre el correu d'entrada que permetran triar diferents tipus d'accions a realitzar.
+
+*   **Resposta**
+    *   Tracta les respostes dels tiquets, comprovant que segueixen un cert patró d'on es pot obtenir el número del tiquet.
+    *   Si l'adreça remitent del correu és coneguda, s'afegeix un comentari en nom seu. Altrament en nom del sol·licitant.
+    *   No s'afegeixen comentaris als tiquets tancats (és el comportament per defecte del servei de GN6).
+*   **Resposta Reobrint**
+    *   Reobre els tiquets tancats quan s'hi rep un resposta si està dins el termini establert pel servei.
+    *   És una extensió del filtre **Resposta**.
+*   **Nou**
+    *   Tracta els correus que s'han de processar com a nous tiquets.
+    *   Han de tenir una adreça remitent coneguda.
+    *   Es pot configurar la creació dels tiquets amb paràmetres diferents (per exemple, l'equip resolutor) segons el valor de determinades capçaleres de correu.
+*   **Nou Extern**
+    *   Crea tiquets per adreces desconegudes amb un usuari predeterminat per configuració.
+    *   És una extensió del filtre **Nou**.
---
+++ settings_sample.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import logging
+import tempfile
+import os
+
+# Utilitzeu això si voleu fer servir diverses instàncies a la mateixa
+# maquina. Cada una necessita el seu directori temporal:
+#
+# tempfile.tempdir = os.environ['HOME'] + "/tmp"
+
+settings = {
+
+  # Usuaris SOA i GN6
+  "username_soa" : "xxx",
+  "password_soa" : "xxx",
+  "username_gn6" : "xxx",
+  "password_gn6" : "xxx",
+
+  # Instància de GN6 on voleu crear els tiquets
+  "domini": "999",
+
+  # Equip resolutor per defecte amb què s'han de crear els tiquets
+  "equip_resolutor_nous": "99999",
+
+  # Usuari amb què es crearan els tiquets pels correus desconeguts
+  # (únicament si teniu activat el FiltreNouExtern)
+  "usuari_extern": "11763",
+
+  # Valors amb què es crearan els tiquets dependent de les capçaleres
+  # del correu rebut. A part de l'equipResolutor, es poden canviar
+  # tots els paramatres documentats al servei SOA de creació de tiquets
+  # (prioritat, tipus...)
+  "valors_defecte": [
+    {
+      "order"    : ['Resent-To','To'],
+      "match"    : "^webmaster@unitat\.upc\.edu$",
+      "defaults" : {"equipResolutor": "11111"}
+    },
+    {
+      "order"    : ['Resent-From','From'],
+      "match"    : "^nom\.cognom@upc\.edu$",
+      "defaults" : {"equipResolutor": "11113"}
+    },
+    {
+      "order"    : ['Resent-From'],
+      "match"    : "^nom@unitat\.upc\.edu$",
+      "defaults" : {"equipResolutor": "11112"}
+    },
+    {
+      "order"    : ['Subject'],
+      "match"    : ".*URGENT",
+      "defaults" : {"urgencia": "GRAVETAT_ALTA"}
+    }
+  ],
+
+  # Filtres actius. També podem utilitzar:
+  # - filtres.reply_reobrint.FiltreRepyReobrint (reobre tiquets tancats)
+  # - filtres.nou_extern.FiltreNouExtern (obre tiquets de correus desconeguts)
+  "filtres": [
+    "filtres.reply.FiltreReply",
+    "filtres.nou.FiltreNou"
+  ],
+
+  # Patró per detectar el número de tiquet
+  "regex_reply": ".*?R[eEvV]:.*?\[Suport Unitat ([\d]+)\]",
+
+  # Patró per detectar si es tracta d'un comentari privat
+  # (també cal modificar la plantilla corresponent a GN6)
+  "regex_privat": "(?i)\(comentari privat\)",
+
+  # Correus addicionals propis de cada unitat que no es troben al servei
+  # d'Identitat Digital UPC. Per cada correu cal indicar quin usuari li
+  # correspon per crear el tiquet.
+  "mails_addicionals": {
+    "gestio.pfc@escola.upc.edu"  : "nom.cognom",
+    "cap.estudis@escola.upc.edu" : "escola.cap.estudis",
+  },
+
+  # Patrons de diferents correus que equivalen a un mateix usuari
+  # o bé a una referència del propi patró (per exemple, el correu
+  # nom.cognom@upc.edu -> nom.cognom).
+  "patrons_mail_addicionals": {
+    "^root@([a-z0-9.\-]+\.)?unitat\.upc\.e(s|du)$" : "usuari.generic",
+    "^(.*)@upc.edu$"                               : "%s",
+    "^(.*)@upcnet.es$"                             : "%s"
+  },
+
+  # Correus dels que no s'ha de crear cap tiquet per la raó que sigui
+  "mails_no_ticket":[
+    "info.exemple@upc.edu"
+  ],
+
+  # Filtres d'adjunts que no s'han de processar (per exemple, les
+  # signatures que contenen imatges adjuntes). Es poden filtrar
+  # pel nom de l'adjunt o per l'emprempta digital en MD5.
+  "filtrar_attachments_per_nom": [
+    "paic\d+.jpg"
+  ],
+
+  "filtrar_attachments_per_hash": [
+    "76f6a359e98f9e0effc214033373b9cf",
+    "7526d0f3f7864090353c181158b218c3"
+  ],
+
+  # Fitxers de log i grau de detall
+  "log_file"  : tempfile.gettempdir() + "/mailtoticket.log",
+  "log_level" : logging.INFO,
+
+  # Notificació dels informes d'error per correu
+  "notificar_errors"      : True,
+  "notificar_errors_from" : "mailtoticket@unitat.upc.edu",
+  "notificar_errors_to"   : "nom.cognom@unitat.upc.edu",
+  "servidor_mail"         : "relay.upc.edu"
+}
