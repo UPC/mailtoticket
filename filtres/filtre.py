be3d006f69580db4f2203beb5228c6ce830d2326
Jaume Moral <jaumem@fib.upc.edu>
2015-12-22 16:50:11 +0100

    Al fer el canvi d'id a id_attachment m'havia deixat una cosa

--- filtres/filtre.py
+++ filtres/filtre.py
@@ -76,7 +76,7 @@ class Filtre(object):
       return html
     html+="<br><br>Attachments:<ul>"
     for id_attachment in ids:
-      a=ids[id]
+      a=ids[id_attachment]
       url=self.url_attachment(id_attachment)
       html+="<li><a href=\"%s\">%s (%s)</a>" % (url,a.get_filename(),a.get_content_type())
     html+="</ul>"
