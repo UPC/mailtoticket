b3bb6623074b837cc1875dd3c9996c0c8d7fd57b
Alex Muntada <alexm@alexm.org>
2016-02-13 23:04:11 +0100

    Adjunta el missatge original als informes d'error
    Closes #54

--- correu.py
+++ correu.py
@@ -1,19 +1,30 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.message import MIMEMessage
 import smtplib
 import settings
 import logging
 logger = logging.getLogger(__name__)
 
-def enviar(text):
+def enviar(text, orig):
   try:
+    msgid=orig['Message-Id']
     de=settings.get("notificar_errors_from");
     a=settings.get("notificar_errors_to");
     host=settings.get("servidor_mail");
     subject="Informe d'errors de MailToTicket"
+    msg = MIMEMultipart('mixed')
+    msg['Subject'] = subject
+    msg['From'] = de
+    msg['To'] = a
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEMessage(orig, 'rfc822')
+    msg.attach(part1)
+    msg.attach(part2)
     server = smtplib.SMTP(host, 25)
-    text_ascii=text.encode('ascii','ignore')
-    msg = "From: %s\nTo: %s\nSubject:%s\n\n%s" % (de,a,subject,text_ascii)
-    server.sendmail(de, a, msg)
-    logger.info("Informe d'errors enviat per mail")
+    server.sendmail(de, a, msg.as_string())
+    server.quit()
+    logger.info("Informe d'errors de %s enviat per correu" % msgid)
   except Exception, e:
     logger.info(e)
-    logger.info("No s'ha pogut enviar l'informe d'error per correu")
+    logger.info("No s'ha pogut enviar per correu l'informe d'errors de %s" % msgid)
