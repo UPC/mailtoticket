92579e9e06f8d96c627b1d459531091720d58d3a
Alex Muntada <alexm@alexm.org>
2016-02-27 20:38:18 +0100

    Passa el PEP8 per settings.py

--- settings.py
+++ settings.py
@@ -3,16 +3,19 @@ El modul de settings s'importa i s'accedeix als valors amb settings.get("clau")
 Abans d'accedir podem carregar la configuracio o agafara la per defecte
 """
 
-def load(module="settings_default"):
-  global settings
-  m=__import__(module,"settings")
-  settings=m.settings
 
-def get(clau):  
-  if not 'settings' in globals():
-    load()
-  global settings  
-  try:
-    return settings[clau]
-  except:
-    return None
+def load(module="settings_default"):
+    global settings
+    m = __import__(module, "settings")
+    settings = m.settings
+
+
+def get(clau):
+    if 'settings' not in globals():
+        load()
+
+    global settings
+    try:
+        return settings[clau]
+    except:
+        return None
