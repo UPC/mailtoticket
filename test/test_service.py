7027127ee15447f93c568956c4393aed02bd802e
Angel Aguilera <angel.aguilera@gmail.com>
2016-03-03 17:31:10 +0100

    Afegits tests del metode resultat_erroni

---
+++ test/test_service.py
@@ -0,0 +1,16 @@
+import unittest
+from soa.service import SOAService
+
+
+class TestService(unittest.TestCase):
+
+    def test_resultat_erroni_true(self):
+        resultat = {'codiRetorn': "2"}
+        self.assertTrue(SOAService.resultat_erroni(resultat))
+
+    def test_resultat_erroni_false(self):
+        resultat = {'codiRetorn': "1"}
+        self.assertFalse(SOAService.resultat_erroni(resultat))
+
+if __name__ == '__main__':
+    unittest.main()
