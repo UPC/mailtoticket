20a648846f1a594e04c6be899458296adae83d57
Jaume Moral <jaume.moral@gmail.com>
2016-09-28 23:58:48 +0200

    isoformat

--- test/test_mailticket.py
+++ test/test_mailticket.py
@@ -30,9 +30,7 @@ class TestMailTicket(unittest.TestCase):
 
     def test_get_date(self):
         d = self.mail.get_date()
-        print d
-        d.replace(tzinfo=None)
-        print d
+        print d.isoformat
         self.assertEquals("28/09/2016 10:24", d.strftime("%d/%m/%Y %H:%M"))
 
     def test_get_date_invalid_format(self):
