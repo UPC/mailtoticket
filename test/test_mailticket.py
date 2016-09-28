7fc1bb509007b026c33fa4aaa17bfd5133c75c1a
Angel Aguilera <angel.aguilera@gmail.com>
2016-09-28 11:04:57 +0200

    Afegits tests per a get_date()

--- test/test_mailticket.py
+++ test/test_mailticket.py
@@ -1,5 +1,6 @@
 import unittest
 import settings
+import datetime
 from mock import patch, mock_open
 from mailticket import MailTicket
 import __builtin__
@@ -9,8 +10,9 @@ class TestMailTicket(unittest.TestCase):
 
     def setUp(self):
         settings.init()
-        with patch.object(__builtin__, 'open',
-                          mock_open(read_data="From: foo@example.com\n\n")):
+        data = "From: foo@example.com\n" \
+               "Date: Tue, 28 Sep 2016 10:24:09 +0200 (CEST)\n\n"
+        with patch.object(__builtin__, 'open', mock_open(read_data=data)):
             with open('foo') as fp:
                 self.mail = MailTicket(fp)
 
@@ -29,6 +31,19 @@ class TestMailTicket(unittest.TestCase):
     def test_mails_no_ticket_0004(self):
         self.mail.mails_no_ticket = ['^.*@example\.com$']
         self.assertFalse(self.mail.cal_tractar())
+
+    def test_get_date(self):
+        d = self.mail.get_date()
+        self.assertEquals("28/09/2016 10:24", d.strftime("%d/%m/%Y %H:%M"))
+
+    def test_get_date_invalid_format(self):
+        # Un missatge amb la data en format "Apple Mail"
+        data = "Date: 9/23/2016 11:04:10 AM\n\n"
+        with patch.object(__builtin__, 'open', mock_open(read_data=data)):
+            with open('foo') as fp:
+                apple_mail = MailTicket(fp)
+
+        self.assertIsInstance(apple_mail.get_date(), datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()
