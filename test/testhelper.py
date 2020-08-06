df995ddc96fdabe2cdcabb7b91bb4146ecf1b017
Jaume Moral <jaumem@fib.upc.edu>
2020-08-07 01:00:17 +0200

    Canvis per passar el pycodestyle

--- test/testhelper.py
+++ test/testhelper.py
@@ -2,12 +2,13 @@ from mailticket import MailTicket
 import os
 from io import BytesIO
 
+
 def llegir_mail(msgfile):
     fp = open(os.path.dirname(__file__) + "/mails/" + msgfile, "rb")
     mail_ticket = MailTicket(fp)
     fp.close()
     return mail_ticket
 
+
 def string_to_mail(data):
     return BytesIO(data.encode())
-
