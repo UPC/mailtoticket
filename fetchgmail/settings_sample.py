84d0b214c3b03dc2c8bb281e1c0800e3062f6a54
Jaume Moral <jaumem@fib.upc.edu>
2020-08-09 19:06:29 +0200

    Llegim correus nous via IMAP amb OAuth2

---
+++ fetchgmail/settings_sample.py
@@ -0,0 +1,6 @@
+user="xxx@upc.edu"
+client_id="xxxxxx"
+client_secret="yyyyyy"
+# El refresh token s'obté executant oauth2.sh
+refresh_token="zzzzzzzz"
+mailtoticket=["python","/mailtoticket/mailtoticket.py"]
